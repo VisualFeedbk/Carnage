eefbcff7947d04b5085c5b701a49feefd5dac4dc
Elliot Peter <149186262+ElliotthePossum@users.noreply.github.com>
2023-12-30 14:49:38 -0500

    Background and fixing colliders
    Added parallax and changed collider shape.

---
+++ Assets/Parallax.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private float startPosition;
+    public GameObject camera;
+    public float parallaxEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position.x;        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = (camera.transform.position.x * parallaxEffect);
+        transform.position = new Vector3(startPosition + distance, transform.position.y, transform.position.z);        
+    }
+}
