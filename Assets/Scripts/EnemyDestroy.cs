661939550509d9897b16b6d59a80439eaaf2291f
Elliot Peter <149186262+ElliotthePossum@users.noreply.github.com>
2024-01-03 11:57:29 -0500

    Enemy Animations & Player Respawn
    Player respawns and enemy disappears.

--- Assets/Scripts/EnemyDestroy.cs
+++ Assets/Scripts/EnemyDestroy.cs
@@ -1,14 +1,26 @@
-using System.Collections;
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyDestroy : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
+    private Animator enemyAnimator;
+
+    void Start()
     {
-        if(collision.gameObject.tag == "Enemy")
+        enemyAnimator = transform.parent.GetComponent<Animator>();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
         {
-            Destroy(collision.gameObject);
+            enemyAnimator.SetTrigger("Destroy");
+            Invoke("DestroyEnemy", 0.5f);
         }
+    }
+
+    public void DestroyEnemy()
+    {
+        Destroy(transform.parent.gameObject);        
     }    
 }
